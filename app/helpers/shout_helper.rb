2062f7b66b585541821c5c08236b4aea4cdc5ada
Scott Goyette <sgoyette@kerberosfoundation.com>
2019-02-13 15:28:45 -0500

    duplicate code in shout_helper & avatar_helper

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,11 +1,4 @@
 module ShoutHelper
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest user.email
-    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
-
-    image_tag gravatar_url
-  end
-
   def like_button(shout)
     if current_user.liked?(shout)
       link_to 'Unlike', unlike_shout_path(shout), method: :delete
@@ -15,7 +8,6 @@ module ShoutHelper
   end
 
   def autolink(text)
-    text.gsub(/[@]\w+/) { |mention| link_to mention, 
-      user_path(mention[1..-1]) }.html_safe
+    text.gsub(/[@]\w+/) { |mention| link_to mention, user_path(mention[1..-1]) }.html_safe
   end
 end

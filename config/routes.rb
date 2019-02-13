93e76eebd8309d50f146ecd7f7232f8f3174cade
Scott Goyette <sgoyette@kerberosfoundation.com>
2019-02-13 15:04:01 -0500

    Unlike button functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,22 +3,23 @@ Rails.application.routes.draw do
     root to: 'dashboards#show'
   end
   root to: 'homes#show'
-  resources :shouts, only: [:create, :show] do
+  resources :shouts, only: %i[create show] do
     member do
-      post 'like' => 'likes#create'
+      post 'like', to: 'likes#create'
+      delete 'unlike', to: 'likes#destroy'
     end
   end
-  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resources :passwords, controller: 'clearance/passwords', only: %i[create new]
   resource :session, only: [:create]
 
-  resources :users, only: [:create, :show] do
+  resources :users, only: %i[create show] do
     resource :password,
-      controller: "clearance/passwords",
-      only: [:create, :edit, :update]
+             controller: 'clearance/passwords',
+             only: %i[create edit update]
   end
 
-  get "/sign_in" => "sessions#new", as: "sign_in"
-  delete "/sign_out" => "sessions#destroy", as: "sign_out"
-  get "/sign_up" => "users#new", as: "sign_up"
+  get '/sign_in', to: 'sessions#new', as: 'sign_in'
+  delete '/sign_out', to: 'sessions#destroy', as: 'sign_out'
+  get '/sign_up', to: 'users#new', as: 'sign_up'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
